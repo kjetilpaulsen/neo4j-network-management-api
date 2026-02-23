8b2d30308edc06a807527389db10a438d1529e42
Kjetil Paulsen <kjetil_paulsen@hotmail.com>
2026-02-23 16:38:53 +0100

    included basic logging in main()

--- app/src/main/kotlin/io/github/noorac/nnma/App.kt
+++ app/src/main/kotlin/io/github/noorac/nnma/App.kt
@@ -1,5 +1,15 @@
 package io.github.noorac.nnma
 
+import org.slf4j.LoggerFactory
+import java.nio.file.Files
+import java.nio.file.Path
+
+private val logger = LoggerFactory.getLogger("io.github.noorac.nnma.Application")
+
 fun main() {
+
+    // Keep in mind this is relative path, must be ran from main directory
+    Files.createDirectories(Path.of("logs"))
+    logger.info("nnma starting up")
     println("Hello World!")
 }
