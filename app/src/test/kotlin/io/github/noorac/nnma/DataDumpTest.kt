278b1c2ea08c189c2018da43573c5e6dc0b4be69
Kjetil Paulsen <kjetil_paulsen@hotmail.com>
2026-02-26 13:05:19 +0100

    added DataDumpTest.kt and stub for class

---
+++ app/src/test/kotlin/io/github/noorac/nnma/DataDumpTest.kt
@@ -0,0 +1,12 @@
+package io.github.noorac.nnma
+
+import org.junit.jupiter.api.Assertions.*
+import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.io.TempDir
+import java.io.ByteArrayInputStream
+import java.net.URI
+import java.nio.file.Files
+import java.nio.file.Path
+
+class DataDumpTest {
+}
