695a0ab845636a2bd9e4fc8f14064142d4b19450
Kjetil Paulsen <kjetil_paulsen@hotmail.com>
2026-02-26 13:14:18 +0100

    fixed AppTest so it doesnt run main, also added test for checking if file exists without actually downloading

--- app/src/test/kotlin/io/github/noorac/nnma/DataDumpTest.kt
+++ app/src/test/kotlin/io/github/noorac/nnma/DataDumpTest.kt
@@ -9,4 +9,32 @@ import java.nio.file.Files
 import java.nio.file.Path
 
 class DataDumpTest {
+    @TempDir
+    lateinit var tmp: Path
+
+    @Test
+    fun `returns existing file without downloading`() {
+        val dataDir = tmp.resolve("data")
+        Files.createDirectory(dataDir)
+
+        val target = dataDir.resolve("x.dump")
+        Files.writeString(target, "already-here")
+
+        var called = false
+        val downloader = Downloader {
+            called = true
+            DownloadResponse(200, ByteArrayInputStream("new".toByteArray()))
+        }
+
+        val out = DataDump.ensurePresent(
+            dataDir = dataDir,
+            dumpId = "x.dump",
+            dumpUrl = URI.create("http://example.invalid/x.dump"),
+            downloader = downloader,
+        )
+
+        assertEquals(target, out)
+        assertFalse(called)
+        assertEquals("already-here", Files.readString(target))
+    }
 }
